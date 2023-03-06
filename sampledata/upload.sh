0988199bb0e96e0e01e6b6c23e929056b79749a7
Brett Kuskie <fullaxx@gmail.com>
2023-03-06 04:53:38 -0700

    Initial commit of packets schema

---
+++ sampledata/upload.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Upload ndjson file
+
+if [ "$#" -ne "2" ]; then
+  echo "$0: <INDEX> <FILE>"
+  exit 1
+fi 
+
+if [ -z "${PASS}" ]; then
+  echo "Set PASS in the environment!"
+  echo "Use /etc/elasticsearch/passwords to find the password for elastic"
+  exit 2
+fi
+
+HOST=${HOST:-localhost}
+INDEX="$1"
+FILE="$2"
+
+if [ ! -r ${FILE} ]; then
+  echo "${FILE} is not readable!"
+  exit 3
+fi
+
+CONVERT=`mktemp`
+while read LINE; do
+  echo '{"index" : {}}' >>${CONVERT}
+  echo ${LINE} >>${CONVERT}
+done <${FILE}
+
+curl -k -s -u elastic:${PASS} -XPOST http://${HOST}:9200/${INDEX}/_bulk?pretty\&refresh=true -H "Content-Type: application/x-ndjson" --data-binary @${CONVERT} 2>&1
+rm ${CONVERT}
